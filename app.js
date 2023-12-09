9712306b3a422881dc4434d0e4ff619a1e49ff74
tanishq2033 <99872122+tanishq2033@users.noreply.github.com>
2023-12-09 23:32:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => {
+    observer.observe(el);
+});
+  
+
+
+
+
+
+
+
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tl = gsap.timeline();
+
+  // Animation for the first section
+  tl.from(".page1 h1", { opacity: 0, y: 50, duration: 1 });
+
+  // Animation for the second section
+  tl.from(".page2 h1, .page2 .tt", { opacity: 0, y: 50, duration: 1 });
+
+  // Animation for the third section
+  tl.from("#image-track .image", { opacity: 0, y: 50, stagger: 0.2, duration: 1 });
+
+  // Animation for the fourth section
+  tl.from(".page4", { opacity: 0, y: 50, duration: 1 });
+
+  // Set up ScrollTrigger for smooth scrolling
+  gsap.utils.toArray("section").forEach((section, index) => {
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top center",
+      end: "bottom center",
+      onToggle: (self) => {
+        if (self.isActive) {
+          tl.seek(index * tl.duration());
+        }
+      },
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
